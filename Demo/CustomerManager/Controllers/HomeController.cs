c4067b205914bc750f982138f3f02bae67bf24f7
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-02-12 23:02:37 +0100

    Forms Beispiel

--- Demo/CustomerManager/Controllers/HomeController.cs
+++ Demo/CustomerManager/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace CustomerManager.Controllers
 {
@@ -11,7 +7,6 @@ namespace CustomerManager.Controllers
         public ViewResult Index()
         {
             ViewBag.Title = "Home Page";
-
             return View();
         }
     }
--- Demo/CustomerManager/Models/Customer.cs
+++ Demo/CustomerManager/Models/Customer.cs
@@ -1,10 +1,23 @@
-﻿namespace CustomerManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerManager.Models
 {
     public class Customer
     {
+        [Display(Name = "Kundennummer")]
         public int Id { get; set; }
+
+        [Display(Name = "Vorname")]
+        [Required(ErrorMessage = "Vorname muss angegeben werden")]
+        [StringLength(200)]
         public string FirstName { get; set; }
+
+        [Display(Name = "Nachname")]
+        [Required(ErrorMessage = "Nachname muss angegeben werden")]
+        [StringLength(200)]
         public string LastName { get; set; }
+
+        [Display(Name = "E-Mail Adresse")]
         public string Mail { get; set; } 
     }
 }
