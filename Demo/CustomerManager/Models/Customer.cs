741135e33013994e0d3b9e850b2513d66c1b64f4
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-02-13 13:01:24 +0100

    Forms Scaffolding: CRUD with EF

--- Demo/CustomerManager/Models/Customer.cs
+++ Demo/CustomerManager/Models/Customer.cs
@@ -1,10 +1,24 @@
-﻿namespace CustomerManager.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomerManager.Models
 {
     public class Customer
     {
+        [Display(Name = "Kundennummer")]
         public int Id { get; set; }
+
+        [Display(Name= "Vorname")]
+        [Required(ErrorMessage = "Bitte geben Sie Ihren Vornamen an.")]
+        [StringLength(200)]
         public string FirstName { get; set; }
+
+        [Display(Name = "Nachname")]
+        [Required(ErrorMessage = "Bitte geben Sie Ihren Nachnamen an.")]
+        [StringLength(200)]
         public string LastName { get; set; }
+
+        [Display(Name = "Mail")]
         public string Mail { get; set; } 
     }
 }
