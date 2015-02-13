df7eb590f55144d2e934833395cd7eff18788444
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-02-13 10:35:40 +0100

    Beispiel Action Filter: RequestTimingFilter

---
+++ Demo/CustomerManager/App_Start/RequestTimingFilter.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace CustomerManager
+{
+    /// <summary>
+    /// Filter to display the execution time of both the action and result
+    /// </summary>
+    public class RequestTimingFilter : FilterAttribute, IActionFilter, IResultFilter
+    {
+        private static Stopwatch GetTimer(ControllerContext context, string name)
+        {
+            var key = string.Format("__timer__{0}", name);
+            if (context.HttpContext.Items.Contains(key))
+            {
+                return (Stopwatch)context.HttpContext.Items[key];
+            }
+
+            var result = new Stopwatch();
+            context.HttpContext.Items[key] = result;
+            return result;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            GetTimer(filterContext, "action").Start();
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            GetTimer(filterContext, "action").Stop();
+        }
+
+        public void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            GetTimer(filterContext, "render").Start();
+        }
+
+        public void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            var renderTimer = GetTimer(filterContext, "render");
+            renderTimer.Stop();
+
+            var actionTimer = GetTimer(filterContext, "action");
+            var response = filterContext.HttpContext.Response;
+
+            if (response.ContentType == "text/html")
+            {
+                response.Write(
+                    string.Format(
+                    "<b>Action '{0} :: {1}'<br /> Execute: {2}ms, Render: {3}ms.</b>",
+                    filterContext.RouteData.Values["controller"],
+                    filterContext.RouteData.Values["action"],
+                    actionTimer.ElapsedMilliseconds,
+                    renderTimer.ElapsedMilliseconds));
+            }
+        }
+    }
+}
